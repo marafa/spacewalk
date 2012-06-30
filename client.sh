5b0b44a69c341c1db0ae69c0b06da5332021113f
محمد عرفة <marafa@notebook>
2012-06-29 20:37:40 -0400

    added repocheck before deleting old CentOS repos

--- client.sh
+++ client.sh
@@ -27,13 +27,11 @@ grep $spacewalk /etc/hosts > /dev/null 2>&1
 
 rpm -Uvh http://spacewalk.redhat.com/yum/1.7/RHEL/$version/$machine/spacewalk-client-repo-1.7-5.el$version.noarch.rpm
 
-[ "$version" -eq "5" ] && rpm -Uvh http://dl.fedoraproject.org/pub/epel/$version/$machine/python-hashlib-20081119-4.el5.$machine.rpm
+[ "$version" -eq "5" ] && rpm -Uvh http://dl.fedoraproject.org/pub/epel/$version/$machine/python-hashlib-20081119-4.el5.$machine.rpm || rpm -Uvh http://ftp.osuosl.org/pub/fedora-epel/6/i386/epel-release-6-7.noarch.rpm
 
 yum -y install rhn-setup yum-rhn-plugin python-dmidecode yum-security.noarch python-hashlib
 
 rpm -Uvh http://$spacewalk/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm --force
-
-rm -rf /etc/yum.repos.d/CentOS-*.repo
 
 sed -i 's|sslCACert=/usr/share/rhn/RHNS-CA-CERT|sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT|g' /etc/sysconfig/rhn/up2date 
 sed -i 's|serverURL=.*$|serverURL=https://$spacewalk/XMLRPC|g' /etc/sysconfig/rhn/up2date
@@ -41,3 +39,11 @@ sed -i 's|serverURL=.*$|serverURL=https://$spacewalk/XMLRPC|g' /etc/sysconfig/rh
 echo "`date` INFO: Registering with $spacewalk"
 #activation key is calculate but is based on spacewalk-common-channels' ini file
 rhnreg_ks --activationkey=1-$os$version-$machine --serverUrl=https://$spacewalk/XMLRPC --force
+
+rhn-channel -l > /dev/null
+if [ $? -eq 0 ] 
+then
+	rm -rf /etc/yum.repos.d/CentOS-*.repo
+	rm -rf /etc/yum.repos.d/spacewalk-client*repo
+fi
+
