4bb9798717056c592aebf5808e0e41c6c3831f35
محمد عرفة <marafa@notebook>
2012-06-29 20:41:37 -0400

    comments

--- client.sh
+++ client.sh
@@ -6,7 +6,7 @@
 machine=`uname -m`
 spacewalk=spacewalk.vm.desktop.us
 ip=192.168.1.11
-
+#version
 if [ -s /etc/centos-release ] 
 then
 	os=centos
@@ -18,6 +18,7 @@ fi
 
 ###main
  
+#clean up before we start
 yum clean all
 yum -y erase rhn-org-trusted-ssl-cert-1.0-1.noarch
 
@@ -29,10 +30,13 @@ rpm -Uvh http://spacewalk.redhat.com/yum/1.7/RHEL/$version/$machine/spacewalk-cl
 
 [ "$version" -eq "5" ] && rpm -Uvh http://dl.fedoraproject.org/pub/epel/$version/$machine/python-hashlib-20081119-4.el5.$machine.rpm || rpm -Uvh http://ftp.osuosl.org/pub/fedora-epel/6/i386/epel-release-6-7.noarch.rpm
 
+#install rhn tools
 yum -y install rhn-setup yum-rhn-plugin python-dmidecode yum-security.noarch python-hashlib
 
+#install our cert
 rpm -Uvh http://$spacewalk/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm --force
 
+#now that /etc/sysconfig/rhn/up2date exists, lets customise it
 sed -i 's|sslCACert=/usr/share/rhn/RHNS-CA-CERT|sslCACert=/usr/share/rhn/RHN-ORG-TRUSTED-SSL-CERT|g' /etc/sysconfig/rhn/up2date 
 sed -i 's|serverURL=.*$|serverURL=https://$spacewalk/XMLRPC|g' /etc/sysconfig/rhn/up2date
 
@@ -40,10 +44,11 @@ echo "`date` INFO: Registering with $spacewalk"
 #activation key is calculate but is based on spacewalk-common-channels' ini file
 rhnreg_ks --activationkey=1-$os$version-$machine --serverUrl=https://$spacewalk/XMLRPC --force
 
+#we good?
 rhn-channel -l > /dev/null
 if [ $? -eq 0 ] 
 then
+#clean up old and duplicate repos
 	rm -rf /etc/yum.repos.d/CentOS-*.repo
 	rm -rf /etc/yum.repos.d/spacewalk-client*repo
 fi
-
