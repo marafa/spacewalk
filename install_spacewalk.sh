72d50991032b9a2abb5b64f180a04b2fb9d4aad8
root <root@spacewalk.marafa.vm>
2015-01-13 16:15:49 +0000

    add ssl cert update

--- install_spacewalk.sh
+++ install_spacewalk.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+yum upgrade ca-certificates --disablerepo=epel -y
+
 yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum -y install deltarpm yum-presto screen alpine git
 yum -y install http://yum.spacewalkproject.org/2.2/RHEL/6/x86_64/spacewalk-repo-2.2-1.el6.noarch.rpm
+
+
+yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm deltarpm yum-presto screen alpine git http://yum.spacewalkproject.org/2.2/RHEL/6/x86_64/spacewalk-repo-2.2-1.el6.noarch.rpm
+
 yum -y update
 
 cd
