8715f17e45ace4bbc9821f1c3577ee346dc3ad08
root <root@spacewalk.marafa.vm>
2015-02-05 01:52:10 +0000

    convert to procedures

--- install_spacewalk.sh
+++ install_spacewalk.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
+do_yum(){
 yum upgrade ca-certificates --disablerepo=epel -y
-
-yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-yum -y install deltarpm yum-presto screen alpine git
-yum -y install http://yum.spacewalkproject.org/2.2/RHEL/6/x86_64/spacewalk-repo-2.2-1.el6.noarch.rpm
-
 
 yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm deltarpm yum-presto screen alpine git http://yum.spacewalkproject.org/2.2/RHEL/6/x86_64/spacewalk-repo-2.2-1.el6.noarch.rpm
 
 yum -y update
+}
 
+do_git(){
 cd
 if [ -d /root/bin ]
 then
@@ -25,6 +23,7 @@ then
 	mv /root/spacewalk /root/bin
 fi
 cp /root/bin/jpackage*repo /etc/yum.repos.d/jpackage-generic.repo
+}
 
 hmm(){
 ##from https://fedorahosted.org/spacewalk/wiki/PostgreSQLServerSetup
@@ -43,12 +42,16 @@ service iptables stop
 chkconfig iptables off
 }
 
-firewall
-
+postgresql(){
 yum -y install spacewalk-setup-postgresql
 yum -y install spacewalk-postgresql
+}
+
+do_yum
+do_git
+firewall
+postgresql
 
 cd /root/bin
 export LANG=en_US.UTF-8
 spacewalk-setup --disconnected --answer-file=spacewalk.answer
-
