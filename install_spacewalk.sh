7917fe1c3a02ece6b489d79c2f87140d01011c3b
marafa <bugzilla@in-egypt.net>
2015-11-28 22:07:14 -0500

    yum clean all

--- install_spacewalk.sh
+++ install_spacewalk.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 do_yum(){
+	
+yum clean all
 yum -y upgrade ca-certificates --disablerepo=epel
 
 #yum -y install http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm deltarpm yum-presto screen alpine git http://yum.spacewalkproject.org/2.2/RHEL/6/x86_64/spacewalk-repo-2.2-1.el6.noarch.rpm createrepo repoview
